9ef7f4d86d4781059e7f8a739078eba2ccc15077
prcsueb <premang.raithatha.1@gmail.com>
2024-10-03 13:07:44 -0700

    Completed Design-4

---
+++ Design Twitter.cpp
@@ -0,0 +1,49 @@
+//time complexity: O(n) spaace complexity: O(n)
+
+class Twitter {
+    unordered_map<int, unordered_set<int>> following;
+    unordered_map<int, vector<pair<int,int>>> tweets;
+    int time;
+public:
+    Twitter() {
+        time = 0;
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        tweets[userId].push_back({time, tweetId});
+        time++;
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        following[userId].insert(userId);
+        vector<int> result;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        for(int user : following[userId]) {
+            
+            int size = tweets[user].size();
+            for(int i = size-1; i >= 0 && i >= size-10; i--) {
+                
+                pq.push(tweets[user][i]);
+                if(pq.size() > 10) {
+                    pq.pop();
+                }
+            }
+        }
+        int size = pq.size();
+        for(int i = 0; i < size; i++) {
+            result.push_back(pq.top().second);
+            pq.pop();
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        following[followerId].insert(followerId);
+        following[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        following[followerId].erase(followeeId);
+    }
+};
